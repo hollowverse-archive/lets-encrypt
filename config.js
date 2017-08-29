9ab4fd28afec1bf429d030ed704afefac4202060
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-08-29 19:15:36 +0300

    Support www. domain

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const DOMAIN = 'hollowverse.com';
+const DOMAIN = 'hollowverse.com,www.hollowverse.com';
 const EMAIL = 'mk@hollowverse.com';
 const CERT_NAME = `lets-encrypt-${DOMAIN}`;
 const STORAGE_BUCKET_ID = 'letsencrypt-hollowverse';
