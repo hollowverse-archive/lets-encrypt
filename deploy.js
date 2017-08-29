56ffb9a29e89c806500a5b50078f47efa41ae174
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-08-29 19:22:37 +0300

    Run tsc in PRs and regular builds

--- deploy.js
+++ deploy.js
@@ -6,7 +6,15 @@ const executeCommands = require('@hollowverse/common/helpers/executeCommands');
 const retryCommand = require('@hollowverse/common/helpers/retryCommand');
 const writeEnvFile = require('@hollowverse/common/helpers/writeEnvFile');
 
-const { ENC_PASS_LETS_ENCRYPT, ENC_PASS_TRAVIS, PROJECT, BRANCH } = shelljs.env;
+const {
+  ENC_PASS_LETS_ENCRYPT,
+  ENC_PASS_TRAVIS,
+  PROJECT,
+  BRANCH,
+  IS_PULL_REQUEST,
+} = shelljs.env;
+
+const isPullRequest = IS_PULL_REQUEST !== 'false';
 
 const secrets = [
   {
@@ -20,25 +28,32 @@ const secrets = [
 ];
 
 async function main() {
+  let commands;
+  commands = ['yarn test'];
+
   if (BRANCH !== 'master') {
     console.info('Not on master branch. Nothing to do.');
-    process.exit(0);
+  } else if (isPullRequest === true) {
+    console.info('Skipping deployment commands in PRs');
+  } else {
+    commands = [
+      ...commands,
+      () => writeEnvFile('lets-encrypt', shelljs.env, './env.json'),
+      () => decryptSecrets(secrets, './secrets'),
+      `gcloud auth activate-service-account --key-file secrets/gcloud.travis.json`,
+      // Remove Travis key file so it does not get deployed with the service
+      () => {
+        shelljs.rm('./secrets/gcloud.travis.json*');
+        return 0;
+      },
+      () =>
+        retryCommand(
+          `gcloud app deploy app.yaml --project ${PROJECT} --version ${BRANCH} --quiet`,
+        ),
+    ];
   }
 
-  const code = await executeCommands([
-    () => writeEnvFile('lets-encrypt', shelljs.env, './env.json'),
-    () => decryptSecrets(secrets, './secrets'),
-    `gcloud auth activate-service-account --key-file secrets/gcloud.travis.json`,
-    // Remove Travis key file so it does not get deployed with the service
-    () => {
-      shelljs.rm('./secrets/gcloud.travis.json*');
-      return 0;
-    },
-    () =>
-      retryCommand(
-        `gcloud app deploy app.yaml --project ${PROJECT} --version ${BRANCH} --quiet`,
-      ),
-  ]);
+  const code = await executeCommands(commands);
 
   process.exit(code);
 }
